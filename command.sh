e4d07ef1d565ff11a45afa303ff5a8260eab6b8e
rrichajalota <rrichajalota@gmail.com>
2017-05-21 10:15:03 +0530

    cases added

--- command.sh
+++
@@ -1,4 +0,0 @@
-pip install -r floyd_requirements.txt
-cp -r /code/* /output
-cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/PHS98Y7t8YSMkbRkse3jfB' --NotebookApp.token='' --NotebookApp.allow_origin='*'
